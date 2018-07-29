bd121f8833cee7047dbfe6330d779f315e18f012
心叶 <yelloxing@gmail.com>
2018-07-29 12:40:46 +0800

    [7月29日]基本的说明文件

---
+++ src/core.js
@@ -0,0 +1,27 @@
+/*!
+ *
+ * quickES - Help quickly use ES.
+ * https://github.com/yelloxing/quickES
+ * 
+ * author yelloxing 心叶
+ *
+ * version v1.0.0
+ * 
+ * build 2018/07/29
+ *
+ * Copyright yelloxing
+ * Released under the MIT license
+ * 
+ **************************************************************
+ * 
+ *【内容】
+ *
+ * 1.不同浏览器兼容的常用方法
+ *
+ * 2.常用的自定义方法
+ *
+ * 【说明】
+ *
+ * 兼容不同浏览器的接口，提供常用的辅助方法，只是针对常用的，目标是轻量级。
+ *
+ */
