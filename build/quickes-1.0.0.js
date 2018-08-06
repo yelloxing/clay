f0b0daa351df2c9a684348a1c62ab1c142ea2f90
心叶 <yelloxing@gmail.com>
2018-08-06 21:27:28 +0800

    [8月6日]线性比例尺基本功能实现

--- build/quickes-1.0.0.js
+++ build/quickes-1.0.0.js
@@ -545,18 +545,44 @@
         var scope = {};
 
         // 返回比例尺计算后的值
-        function scaleLinear() {
-
+        function scaleLinear(domain) {
+            if (domain && typeof domain === 'number') {
+                if (!scope.scaleCalc) {
+                    throw new Error('You shoud first set the domain and range!');
+                } else if (domain <= scope.domains[0]) {
+                    return scope.ranges[0];
+                } else if (domain >= scope.domains[1]) {
+                    return scope.ranges[1];
+                } else {
+                    return (domain - scope.domains[0]) * scope.scaleCalc + scope.ranges[0];
+                }
+            } else {
+                throw new Error('A number is required!');
+            }
         };
 
         // 获取或设置定义域
-        scaleLinear.domain = function () {
-
+        scaleLinear.domain = function (domains) {
+            if (!domains || domains.constructor !== Array || typeof domains[0] !== 'number' || typeof domains[1] !== 'number') {
+                throw new Error('Unsupported data!');
+            }
+            scope.domains = domains;
+            if (scope.ranges) {
+                scope.scaleCalc = (scope.ranges[1] - scope.ranges[0]) / (scope.domains[1] - scope.domains[0]);
+            }
+            return this;
         };
 
         // 定义或设置值域
-        scaleLinear.range = function () {
-
+        scaleLinear.range = function (ranges) {
+            if (!ranges || ranges.constructor !== Array || typeof ranges[0] !== 'number' || typeof ranges[1] !== 'number') {
+                throw new Error('Unsupported data!');
+            }
+            scope.ranges = ranges;
+            if (scope.domains) {
+                scope.scaleCalc = (scope.ranges[1] - scope.ranges[0]) / (scope.domains[1] - scope.domains[0]);
+            }
+            return this;
         };
 
         return scaleLinear;
