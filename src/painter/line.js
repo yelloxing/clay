1d24afff35a3313d862334258b9bd0b2ebf20c9b
tear520 <3294539252@qq.com>
2018-10-03 16:21:36 +0800

    [10月3日]任意线条绘制（canvas+svg）

--- src/painter/line.js
+++ src/painter/line.js
@@ -10,17 +10,61 @@ var _line = function (config) {
     };
 
     // points代表曲线的点集合[[x,y],[x,y],...]
-    var line = function (points) {
+    // isClose表示曲线是否需要闭合（改变的是计算方法，不会自动闭合）
+    var line = function (points, isClose) {
 
-        if (typeof scope.h === 'number') {
-            var cardinal, i,
+        if (scope.flag || typeof scope.h === 'number') {
+            var hermite, i, j, p1, p2,
                 result = config.init(points[0][0], scope.flag ? points[0][1] : scope.h - points[0][1]);
 
             // cardinal插值法
             if (scope.interpolate === 'cardinal') {
-                cardinal = clay.math.cardinal().setU(scope.t).setP(points);
-                for (i = points[0][0] + scope.dis; i < points[points.length - 1][0]; i += scope.dis)
-                    result = config.draw(result, i, scope.flag ? cardinal(i) : scope.h - cardinal(i));
+                hermite = clay.math.hermite().setU(scope.t);
+
+                // 补足开头和结尾的辅助点
+                points.unshift(isClose ? points[points.length - 1] : points[0]);
+                points.push(isClose ? points[1] : points[points.length - 1]);
+
+                for (i = 1; i < points.length - 2; i++) {
+
+                    // 计算i，i+1段
+                    if (points[i][0] == points[i + 1][0]) {
+
+
+                        p1 = (points[i + 1][0] - points[i - 1][0]) / (points[i + 1][1] - points[i - 1][1]);
+                        p2 = (points[i + 2][0] - points[i][0]) / (points[i + 2][1] - points[i][1]);
+                        // 如果二个x坐标相同，交换坐标轴
+                        if (points[i][1] < points[i + 1][1]) {
+
+                            hermite.setP(points[i][1], points[i][0], points[i + 1][1], points[i + 1][0], p1, p2);
+                            for (j = points[i][1]; j <= points[i + 1][1]; j += scope.dis)
+                                result = config.draw(result, hermite(j), scope.flag ? j : scope.h - j);
+                        } else {
+
+                            hermite.setP(points[i + 1][1], points[i + 1][0], points[i][1], points[i][0], p2, p1);
+                            for (j = points[i][1]; j >= points[i + 1][1]; j -= scope.dis)
+                                result = config.draw(result, hermite(j), scope.flag ? j : scope.h - j);
+                        }
+
+                    } else {
+
+                        p1 = (points[i + 1][1] - points[i - 1][1]) / (points[i + 1][0] - points[i - 1][0]);
+                        p2 = (points[i + 2][1] - points[i][1]) / (points[i + 2][0] - points[i][0]);
+                        if (points[i][0] < points[i + 1][0]) {
+
+                            hermite.setP(points[i][0], points[i][1], points[i + 1][0], points[i + 1][1], p1, p2);
+                            for (j = points[i][0]; j <= points[i + 1][0]; j += scope.dis)
+                                result = config.draw(result, j, scope.flag ? hermite(j) : scope.h - hermite(j));
+                        } else {
+
+                            hermite.setP(points[i + 1][0], points[i + 1][1], points[i][0], points[i][1], p2, p1);
+                            for (j = points[i][0]; j >= points[i + 1][0]; j -= scope.dis)
+                                result = config.draw(result, j, scope.flag ? hermite(j) : scope.h - hermite(j));
+                        }
+
+                    }
+                }
+
             }
 
             // 默认或错误设置都归结为line
