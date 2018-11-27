180110f6b4a81076bc4835645684115dddc42530
心叶 <yelloxing@gmail.com>
2018-11-27 11:31:22 +0800

    整理文档 图形

--- src/graphics/line.js
+++ src/graphics/line.js
@@ -1,44 +1,27 @@
-// 任意曲线
+// 曲线
 var _line = function (painter) {
 
-    var scope = {},
+    var scope = {
+        d: 5
+    },
         hermite = clay.hermite().setU(-1);
 
     /**
-     * 绘制任意曲线
+     * 绘制曲线
      */
     var line = function (points) {
         var i = 0, temp = "M" + points[0][0] + "," + points[0][1];
-        var sqrt2,
-            cos, sin,
-            cx, cy,
-            x, y,
-            leftP, rightP;
+        var l, r;
         for (; i < points.length - 1; i++) {
-            sqrt2 = Math.sqrt((points[i][0] - points[i + 1][0]) * (points[i][0] - points[i + 1][0]) + (points[i][1] - points[i + 1][1]) * (points[i][1] - points[i + 1][1]));
-            sin = -(points[i][1] - points[i + 1][1]) / 2 / sqrt2;
-            cos = (points[i + 1][0] - points[i][0]) / 2 / sqrt2;
-            cx = (points[i][0] + points[i + 1][0]) / 2;
-            cy = (points[i][1] + points[i + 1][1]) / 2;
-            x = points[i == 0 ? 0 : i - 1][0];
-            y = points[i == 0 ? 0 : i - 1][1];
-            leftP = [
-                ((x - cx) * cos - (y - cy) * sin + cx).toFixed(7),
-                ((x - cx) * sin + (y - cy) * cos + cy).toFixed(7)
-            ];
-            x = points[i >= points.length - 2 ? points.length - 1 : i + 2][0];
-            y = points[i >= points.length - 2 ? points.length - 1 : i + 2][1];
-            rightP = [
-                ((x - cx) * cos - (y - cy) * sin + cx).toFixed(7),
-                ((x - cx) * sin + (y - cy) * cos + cy).toFixed(7)
-            ];
+            l = i == 0 ? 0 : i - 1;
+            r = i == points.length - 2 ? points.length - 1 : i + 2;
             hermite.setP(
-                (points[i][0] + points[i + 1][0]) * 0.5 - sqrt2 * 0.5, (points[i][1] + points[i + 1][1]) * 0.5,
-                (points[i][0] + points[i + 1][0]) * 0.5 + sqrt2 * 0.5, (points[i][1] + points[i + 1][1]) * 0.5,
-                points[i + 1][0] == leftP[0] ? -9999999 : (points[i + 1][1] - leftP[1]) / (points[i + 1][0] - leftP[0]),
-                points[i][0] == rightP[0] ? 9999999 : (points[i][1] - rightP[1]) / (points[i][0] - rightP[0])
+                points[i][0], points[i][1],
+                points[i + 1][0], points[i + 1][1],
+                (points[i + 1][1] - points[l][1]) / (points[i + 1][0] - points[l][0]),
+                (points[r][1] - points[i][1]) / (points[r][0] - points[i][0])
             );
-            temp = painter(hermite, (points[i][0] + points[i + 1][0]) * 0.5 - sqrt2 * 0.5, (points[i][0] + points[i + 1][0]) * 0.5 + sqrt2 * 0.5, temp, scope.d);
+            temp = painter(hermite, points[i][0], points[i + 1][0], temp, scope.d);
         }
         return temp;
     };
@@ -49,11 +32,17 @@ var _line = function (painter) {
         return line;
     };
 
+    // 设置张弛系数
+    line.setU = function (u) {
+        hermite.setU(u);
+        return line;
+    };
+
     return line;
 
 };
 
-// 采用SVG绘制任意曲线
+// 采用SVG绘制曲线
 clay.svg.line = function () {
     return _line(
         function (
@@ -67,7 +56,7 @@ clay.svg.line = function () {
     );
 };
 
-// 采用Canvas绘制任意曲线
+// 采用Canvas绘制曲线
 clay.canvas.line = function (selector, config) {
     var key,
         obj =
