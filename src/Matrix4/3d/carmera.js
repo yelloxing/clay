e578510182e99561d97757061bd8133db5175e69
心叶 <yelloxing@gmail.com>
2018-12-18 15:04:24 +0800

    视图矩阵（缺乏单元测试和用例测试，文档）

--- src/Matrix4/3d/carmera.js
+++ src/Matrix4/3d/carmera.js
@@ -1,26 +1,107 @@
 // 视图
 clay.carmera = function () {
 
-    var matrix4 = clay.Matrix4();
+    var matrix4,
+        carmera = {
 
-    var carmera = {
-        "lookAt": function (
-            // 视点
-            eX, eY, eZ,
-            // 观察方向
-            dX, dY, dZ,
-            // 上方向
-            upX, upY, upZ
-        ) {
+            // 设置或重置观察"姿势"
+            "lookAt": function (
+                // 视点
+                eX, eY, eZ,
+                // 观察目标中心点
+                cX, cY, cZ,
+                // 上方向
+                upX, upY, upZ
+            ) {
 
+                /**
+                 * 视点移动至原点
+                 */
+                cX -= eX;
+                cY -= eY;
+                cZ -= eZ;
 
+                // 初始化矩阵
+                matrix4 = clay.Matrix4([
+                    1, 0, 0, 0,
+                    0, 1, 0, 0,
+                    0, 0, 1, 0,
+                    -eX, -eY, -eZ, 1
+                ]);
 
-            return carmera;
-        },
-        "value": function () {
-            return matrix4.value();
-        }
-    };
+                /**
+                 * 上方向旋转成Oy轴方向
+                 */
+
+                // 转至xOy平面
+                var sqrt = Math.sqrt(upX * upX + upZ * upZ),
+                    cos = upX / sqrt,
+                    sin = upZ / sqrt;
+                cX = cZ * sin + cX * cos;
+                cZ = cZ * cos - cX * sin;
+
+                matrix4.multiply([
+                    cos, 0, -sin, 0,
+                    0, 1, 0, 0,
+                    sin, 0, cos, 0,
+                    0, 0, 0, 1
+                ]);
+
+                upX = sqrt * (upX > 0 ? 1 : -1);
+
+                // 转至Oy轴上，正方向
+                sqrt = Math.sqrt(upX * upX + upY * upY);
+                cos = upY / sqrt;
+                sin = upX / sqrt;
+                cX = cX * cos - cY * sin;
+                cY = cX * sin + cY * cos;
+
+                matrix4.multiply([
+                    cos, sin, 0, 0,
+                    -sin, cos, 0, 0,
+                    0, 0, 1, 0,
+                    0, 0, 0, 1
+                ]);
+
+                /**
+                 * 观察中心旋转至-Oz轴上
+                 */
+
+                // 转至yOz平面
+                sqrt = Math.sqrt(cX * cX + cZ * cZ);
+                cos = cZ / sqrt;
+                sin = -cX / sqrt;
+
+                matrix4.multiply([
+                    cos, 0, -sin, 0,
+                    0, 1, 0, 0,
+                    sin, 0, cos, 0,
+                    0, 0, 0, 1
+                ]);
+
+                cZ = sqrt * (cZ > 0 ? 1 : -1);
+
+                // 转至Oz轴上，负方向
+                sqrt = Math.sqrt(cY * cY + cZ * cZ);
+                cos = -cX / sqrt;
+                sin = -cZ / sqrt;
+
+                matrix4.multiply([
+                    1, 0, 0, 0,
+                    0, cos, sin, 0,
+                    0, -sin, cos, 0,
+                    0, 0, 0, 1
+                ]);
+
+                return carmera;
+            },
+
+            // 获取视图矩阵数组表示
+            "value": function () {
+                return matrix4.value();
+            }
+
+        };
 
     return carmera;
 
--- src/Matrix4/3d/prespective.js
+++ src/Matrix4/3d/prespective.js
@@ -1,27 +1,27 @@
 // 透视投影
 clay.prespective = function () {
 
-    var matrix4 = clay.Matrix4();
+    var matrix4,
 
-    // 这是一个一点透视
-    var prespective = {
-        "set": function (
-            // 垂直视角，也就是视野的顶面和底面夹角
-            fov,
-            // 视野的宽/高
-            aspect,
-            // 近裁剪面和远裁剪面
-            near, far
-        ) {
+        // 这是一个一点透视
+        prespective = {
+            "set": function (
+                // 垂直视角，也就是视野的顶面和底面夹角
+                fov,
+                // 视野的宽/高
+                aspect,
+                // 近裁剪面和远裁剪面
+                near, far
+            ) {
 
 
 
-            return prespective;
-        },
-        "value": function () {
-            return matrix4.value();
-        }
-    };
+                return prespective;
+            },
+            "value": function () {
+                return matrix4.value();
+            }
+        };
 
     return prespective;
 
