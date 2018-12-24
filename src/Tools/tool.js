e581260bf4dc8d213b5f9d02166f63aca868357e
fujing <1057891494@qq.com>
2018-12-24 14:39:57 +0800

    update getColors

--- src/Tools/tool.js
+++ src/Tools/tool.js
@@ -6,21 +6,94 @@ clay.color = function (color) {
 };
 
 // 获取一组色彩
-clay.getColors = function (num) {
-    if (typeof num == 'number' && num > 3) {
-        var temp = [], flag = 0;
-        for (flag = 1; flag <= num; flag++)
-            temp.push('rgb(' + (Math.random(1) * 230 + 20).toFixed(0) + ',' + (Math.random(1) * 230 + 20).toFixed(0) + ',' + (Math.random(1) * 230 + 20).toFixed(0) + ')');
-        return temp;
+clay.getColors = function (num, range, rgb) {
+    //num：需要的颜色个数
+    //range:数组，取值0-360，色彩范围
+    //rgb: 可选，数组，参考颜色，是一组rgb
+    var temp = (range[1] - range[0]) / num;
+    var s, l;
+    if (rgb) {
+        var hsl = rgb2hsl(rgb[0], rgb[1], rgb[2]);
+        s = hsl[1], l = hsl[2];
     } else {
-        return ['rgb(255,0,0)', 'rgb(0,255,0)', 'rgb(0,0,255)'];
+        s = 0.78, l = 0.4;
     }
-};
+    var array = [];
+    for (var i = 0; i < num; i++) {
+        var rgb = hsl2rgb(temp * i + range[0], s, l);
+        array.push("rgb("+rgb[0]+","+rgb[1]+","+rgb[2]+")");
+    }
+    return array;
+}
 
 // 给一组数据，轮询执行一遍
 clay.loop = function (datas, callback) {
-    var flag = 0, data;
+    var flag = 0,
+        data;
     for (data in datas)
         callback(datas[data], data, flag++);
     return clay;
 };
+
+function rgb2hsl(R, G, B) {
+    var R1 = +R / 255,
+        G1 = +G / 255,
+        B1 = +B / 255;
+    var MAX = Math.max(R1, G1, B1);
+    var MIN = Math.min(R1, G1, B1);
+    var H, S, L;
+    if (MAX === MIN) {
+        H = 0;
+    } else if (MAX === R1) {
+        H = 60 * (G1 - B1) / (MAX - MIN);
+    } else if (MAX === G1) {
+        H = 60 * (B1 - R1) / (MAX - MIN) + 120;
+    } else if (MAX === B1) {
+        H = 60 * (R1 - G1) / (MAX - MIN) + 240;
+    }
+    if (H < 0) {
+        H += 360;
+    }
+    L = (MAX + MIN) / 2;
+    if (L === 0 || MAX === MIN) {
+        S = 0;
+    } else if (L > 0 && L <= 0.5) {
+        S = (MAX - MIN) / (MAX + MIN);
+    } else if (L > 0.5) {
+        S = (MAX - MIN) / (2 - MAX - MIN);
+    }
+    return [+H.toFixed(2), +S.toFixed(2), +L.toFixed(2)];
+}
+
+function hsl2rgb(h, s, l) {
+    var c = (1 - Math.abs(2 * l - 1)) * s;
+    var x = c * (1 - Math.abs((h / 60) % 2 - 1));
+    var m = l - c / 2;
+    var r, g, b;
+    if (h >= 0 && h < 60) {
+        r = (c + m) * 255;
+        g = (x + m) * 255;
+        b = m * 255;
+    } else if (h >= 60 && h < 120) {
+        r = (x + m) * 255;
+        g = (c + m) * 255;
+        b = m * 255;
+    } else if (h >= 120 && h < 180) {
+        r = m * 255;
+        g = (c + m) * 255;
+        b = (x + m) * 255;
+    } else if (h >= 180 && h < 240) {
+        r = m * 255;
+        g = (x + m) * 255;
+        b = (c + m) * 255;
+    } else if (h >= 240 && h < 300) {
+        r = (x + m) * 255;
+        g = m * 255;
+        b = (c + m) * 255;
+    } else if (h >= 300 && h < 360) {
+        r = (c + m) * 255;
+        g = m * 255;
+        b = (x + m) * 255;
+    }
+    return [+r.toFixed(0), +g.toFixed(0), +b.toFixed(0)];
+}
