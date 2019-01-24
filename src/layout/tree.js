1944c1b029f7491f35da6ba2e7dce628c126f9f5
心叶 <yelloxing@gmail.com>
2019-01-24 11:30:27 +0800

    更新tree树布局位置计算中，缺少对父结点的前兄弟位置信息的缺失问题。

--- src/layout/tree.js
+++ src/layout/tree.js
@@ -63,6 +63,21 @@ clay.treeLayout = function () {
                     // 其实，flag==0的分支计算的就是孩子，是没有孩子的叶结点，那是关键
                     alltreedata[pNode.id].top = (alltreedata[pNode.children[0]].top + alltreedata[pNode.children[flag - 1]].top) * 0.5;
                 }
+                
+                // 因为计算孩子的时候
+                // 无法掌握父辈兄弟的情况
+                // 可能会出现父亲和兄弟重叠问题
+                if (alltreedata[pNode.id].top <= beforeDis[deep]) {
+                    var needUp = beforeDis[deep] + 1 - alltreedata[pNode.id].top;
+                    (function doUp(_pid, _deep) {
+                        alltreedata[_pid].top += needUp;
+                        if (beforeDis[_deep] < alltreedata[_pid].top) beforeDis[_deep] = alltreedata[_pid].top;
+                        var _flag;
+                        for (_flag = 0; _flag < alltreedata[_pid].children.length; _flag++) {
+                            doUp(alltreedata[_pid].children[_flag], _deep + 1);
+                        }
+                    })(pNode.id, deep);
+                }
 
                 // 计算好一个结点后，需要更新此刻该层的上边缘
                 beforeDis[deep] = alltreedata[pNode.id].top;
